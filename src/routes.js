3c497a2c6138c02fb90dddb3d707625a12efd8a7
Patrick Chagas <patrick.c.tavare@gmail.com>
2019-07-07 11:03:33 -0300

    chore: add config eslint, prettier and editorconfig

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
+routes.get('/', (req, res) => {
   return res.json({
-    message: "Hello World, my name is Patrick, I'am phone develop"
+    message: "Hello World, my name is Patrick, I'am phone develop",
   });
 });
 
