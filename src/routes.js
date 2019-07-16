27492fe38fc86989d3f42755dabcf51ac4dad4f5
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-07-16 11:46:44 -0300

    create: add listing of the user appointment with services provider and pagination

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderControler from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,6 +21,9 @@ routes.use(authMiddleware); // middleware para validar se o usuario está logado
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderControler.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
