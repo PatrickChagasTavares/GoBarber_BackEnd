1c5516acc39715bed4dfda5f42b38338f4124e37
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-07-17 12:12:06 -0300

    create: configuring enviroment variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'ffd6825a20e8a8',
-    pass: '3195418b01f053',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Gobarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://82ab17ae33d34842a267da3fffb73536@sentry.io/1507192',
+  dsn: process.env.SENTRY_DSN,
 };
